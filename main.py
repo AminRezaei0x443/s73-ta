9db49c9b38f0ca9d89817c9e9bcd35d079549e4e
Amin Rezaei <AminRezaei0x443@gmail.com>
2022-07-04 20:29:09 +0430

    init: initial fastAPI codebase with uvicorn server

---
+++ main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8118, log_level="info")
+
+
+if __name__ == "__main__":
+    main()
